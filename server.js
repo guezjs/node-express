60f639980a9ce48d2cfabc1cd6506e92521221c2
Josue Iniguez <josue.i@forever21.com>
2022-12-10 08:51:05 -0800

    Express Simple REST

--- server.js
+++ server.js
@@ -9,6 +9,48 @@ const app = express();
 
 // configures Morgan to log using the development version, prints additional information to the screen.
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.all('/campsites', (req, res, next) => {
+    res.status = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next(); // passes control of the application routing to the next relevant routing method after this one, otherwise it stops here.
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req,res) => {
+    res.status = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.status = 403
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 // middleware function express.static() will serve files from the public folder.
 // __dirname - special variable in Node, refers to the absolute path of the current directory of the file that it's in.
